adbd44f06df93c3d66b2ec327efd9b9d17982cc7
Benja Heart <visopsys@users.noreply.github.com>
2023-01-31 11:37:21 -0800

    Remove hardcoded vault & token addresses

--- ETH_to_Lisk/index.js
+++ ETH_to_Lisk/index.js
@@ -5,10 +5,10 @@
   const web3 = new Web3("https://rpc.ankr.com/polygon_mumbai");
 
   const privateKey = process.env.PRIVATE_KEY;
-  const vaultContract = "0x303711CFe2C9bD11fc620C2BEE71Db26AC866130";
-  const srcToken = "0x4B7785c2a265D6DF777c1635C1B1B09a6b9fba9d";
+  const vaultContract = process.env.VAULT_ADDRESS;
+  const srcToken = process.env.ERC20_ADDRESS;
   const dstReceiverToken = "lskw6errschsfdrou4q8cdcoz7q79f82mzuso8wqw";
-  const liskTestnetId = "9872347238347";
+  const liskTestnetId = "9872347238347"; // Lisk testnet chain id
   const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
   let contract = new web3.eth.Contract(abi, vaultContract, {
     from: account.address, // default from address
@@ -18,12 +18,7 @@
   web3.eth.defaultAccount = account.address;
 
   contract.methods
-    .transferOutNonEvm(
-      srcToken,
-      liskTestnetId,
-      dstReceiverToken,
-      BigInt(100000000000000000000)
-    )
+    .transferOutNonEvm(srcToken, liskTestnetId, dstReceiverToken, BigInt(100000000000000000000))
     .send({ from: account.address }, function (err, res) {
       if (err) {
         console.log("An error occurred", err);
