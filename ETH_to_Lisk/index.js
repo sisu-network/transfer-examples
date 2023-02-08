c2911d20e5df1a9d53072e1d778c664cc0d4d18c
Benja Heart <visopsys@users.noreply.github.com>
2023-02-07 23:45:24 -0800

    Update ETH -> Lisk transfer amount

--- ETH_to_Lisk/index.js
+++ ETH_to_Lisk/index.js
@@ -1,12 +1,13 @@
-(async () => {
-  const Web3 = require("web3");
-  const { abi } = require("./abi/vaultABI");
-  require("dotenv").config();
-  const web3 = new Web3("https://rpc.ankr.com/polygon_mumbai");
+const Web3 = require("web3");
+const { abi } = require("./abi/vaultABI");
+require("dotenv").config();
+const web3 = new Web3("https://rpc.ankr.com/polygon_mumbai");
 
+(async () => {
   const privateKey = process.env.PRIVATE_KEY;
   const vaultContract = process.env.VAULT_ADDRESS;
   const srcToken = process.env.ERC20_ADDRESS;
+
   const dstReceiverToken = "lskw6errschsfdrou4q8cdcoz7q79f82mzuso8wqw";
   const liskTestnetId = "9872347238347"; // Lisk testnet chain id
   const account = web3.eth.accounts.privateKeyToAccount("0x" + privateKey);
@@ -18,7 +19,7 @@
   web3.eth.defaultAccount = account.address;
 
   contract.methods
-    .transferOutNonEvm(srcToken, liskTestnetId, dstReceiverToken, BigInt(100000000000000000000))
+    .transferOutNonEvm(srcToken, liskTestnetId, dstReceiverToken, BigInt(100000000000000000))
     .send({ from: account.address }, function (err, res) {
       if (err) {
         console.log("An error occurred", err);
